d64292805bc82df87ab13dc9bfc4bb058639f0f3
alex.fan <alex.fan@okchem.com>
2019-07-08 14:34:04 +0800

    cpu hungry task

---
+++ src/Info/event-loop/cpu-hungry-tasks.js
@@ -0,0 +1,14 @@
+let i = 0
+
+let start = Date.now()
+
+function count() {
+  // do a heavy job
+  for (let j = 0; j < 1e9; j++) {
+    i++
+  }
+
+  console.log('Done in ' + (Date.now() - start) + 'ms')
+}
+
+count()
