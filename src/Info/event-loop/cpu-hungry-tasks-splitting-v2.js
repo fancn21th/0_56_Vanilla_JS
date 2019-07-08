d91fe0f14e4af636908107f7f1f9ca2b2c44bf06
alex.fan <alex.fan@okchem.com>
2019-07-08 14:48:55 +0800

    comment

--- src/Info/event-loop/cpu-hungry-tasks-splitting-v2.js
+++ src/Info/event-loop/cpu-hungry-tasks-splitting-v2.js
@@ -6,6 +6,7 @@ let start = Date.now()
 function count() {
   // move the scheduling at the beginning
   if (i < 1e9 - 1e6) {
+    // remember, there’s the in-browser minimal delay of 4ms for many nested setTimeout calls. Even if we set 0, it’s 4ms (or a bit more). So the earlier we schedule it – the faster it runs.
     setTimeout(count) // schedule the new call
   }
 
